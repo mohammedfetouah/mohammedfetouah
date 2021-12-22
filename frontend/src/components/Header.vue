3341a3a19ca4a3c70a42e6ce641bcbcaef1dd8ce
Mohammed Fetouah <Fetouah.m@gmail.com>
2021-12-22 21:10:28 +0100

    push 2

---
+++ frontend/src/components/Header.vue
@@ -0,0 +1,43 @@
+<template>
+    <div class="container">
+        <nav class="navbar navbar-expand-md navbar-light">
+            <a href="#" class="navbar-brand">Réseaux social</a>
+            <button
+                class="navbar-toggler"
+                type="button"
+                data-bs-toggle="collapse"
+                data-bs-target="#toggleMobileMenu"
+                aria-controls="toggleMobileMenu"
+                aria-expanded="false"
+                aria-lable="Toggle navigation"
+            >
+                <span class="navbar-toggler-icon">
+                </span>
+            </button>
+            <div class="collapse navbar-collapse" id="toggleMobileMenu">
+                <ul class="navbar-nav ms-auto text-center">
+                    <li>
+                        <a class="nav-link" href="#"> Signup</a>
+                    </li>
+                    <li>
+                        <a class="nav-link" href="#"> Login</a>
+                    </li>
+                </ul>
+            </div>
+
+        </nav>
+
+
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+
+<style scoped lang="scss">
+
+</style>
